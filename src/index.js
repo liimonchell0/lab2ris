b2a9ba7efa54248ed877474ed103bb773cbd0469
liimonchell0 <96261920+liimonchell0@users.noreply.github.com>
2024-05-19 19:53:44 +0300

    condd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { homedir } from 'os';
+import { readdir, stat } from 'fs';
+import { resolve } from 'path';
 
 const args = process.argv.slice(2);
 const usernameArg = args.find(arg => arg.startsWith('--Lidiya='));
-const username = usernameArg ? usernameArg.split('=')[1] : 'Lidiya';
+const username = usernameArg ? usernameArg.split('=')[1] : 'Guest';
+
+const homeDirectory = homedir();
+let currentWorkingDirectory = homeDirectory;
 
 const printWorkingDirectory = () => {
-    const homeDirectory = homedir();
-    console.log(`You are currently in ${homeDirectory}`);
+    console.log(`You are currently in ${currentWorkingDirectory}`);
 };
 
 const exitHandler = () => {
@@ -18,13 +22,61 @@ const exitHandler = () => {
 console.log(`Welcome to the File Manager, ${username}!`);
 printWorkingDirectory();
 
-// Обработчик для Ctrl+C
-process.on('SIGINT', exitHandler);
+const handleInput = (input) => {
+    const [command, ...params] = input.split(' ');
 
-// Обработчик для команды .exit
-process.stdin.on('data', (data) => {
-    const input = data.toString().trim();
-    if (input === '.exit') {
-        exitHandler();
+    switch (command) {
+        case 'pwd':
+            printWorkingDirectory();
+            break;
+        case 'ls':
+            listFiles(params[0] || currentWorkingDirectory);
+            break;
+        case 'cd':
+            changeDirectory(params[0]);
+            break;
+        case 'exit':
+            exitHandler();
+            break;
+        default:
+            console.log('Invalid input');
     }
+};
+
+const listFiles = (directoryPath) => {
+    readdir(directoryPath, (err, files) => {
+        if (err) {
+            console.log('Operation failed');
+            return;
+        }
+        console.log(files.join('\n'));
+    });
+};
+
+const changeDirectory = (targetDirectory) => {
+    if (!targetDirectory) {
+        console.log('Invalid input');
+        return;
+    }
+
+    const newPath = resolve(currentWorkingDirectory, targetDirectory);
+    if (!newPath.startsWith(homeDirectory)) {
+        console.log('Invalid input');
+        return;
+    }
+
+    stat(newPath, (err, stats) => {
+        if (err || !stats.isDirectory()) {
+            console.log('Operation failed');
+            return;
+        }
+        currentWorkingDirectory = newPath;
+        printWorkingDirectory();
+    });
+};
+
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', (data) => {
+    const input = data.trim();
+    handleInput(input);
 });
